b19792b5f37a7637260b7720e179a3e0d52432b1
Michael Herman <hermanmu@gmail.com>
2018-11-12 06:57:50 -0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dockerizing Django with Postgres, Gunicorn, and Nginx
+
+## Want to learn how to build this?
+
+Check out the [post](https://testdriven.io/dockerizing-django-with-postgres-gunicorn-and-nginx).
+
+## Want to use this project?
+
+1. Fork/Clone
+
+1. Build the images and run the containers:
+
+    ```sh
+    $ docker-compose up -d --build
+    ```
+
+1. Test it out at [http://localhost:1337](http://localhost:1337)
---
+++ app/upload/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
+
+def image_upload(request):
+    if request.method == 'POST' and request.FILES['image_file']:
+        image_file = request.FILES['image_file']
+        fs = FileSystemStorage()
+        filename = fs.save(image_file.name, image_file)
+        image_url = fs.url(filename)
+        return render(request, 'upload.html', {
+            'image_url': image_url
+        })
+    return render(request, 'upload.html')
