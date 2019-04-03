ac2bd51c16dc0dd4ffa035a01e96be9b0640f77d
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-03 15:24:15 +0800

    docker-compose dev and prod files

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from upload.views import image_upload
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', image_upload, name='upload'),
     path('admin/', admin.site.urls),
 ]
+
+# to allow static in development mode. it could be removed when run via nginx
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
